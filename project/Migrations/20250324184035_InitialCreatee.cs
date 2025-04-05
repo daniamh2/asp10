e36fa664d9d475c0dcc68068b9bf506eb2b7a96a
dania <daniahilal@gmail.com>
2025-04-05 15:17:34 +0300

    c

---
+++ project/Migrations/20250324184035_InitialCreatee.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace project.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreatee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "status",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "Categories");
+        }
+    }
+}

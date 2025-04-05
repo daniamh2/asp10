25447155510ccfc488c488c6fe0f7dcf2a96e4d1
dania <daniahilal@gmail.com>
2025-04-05 15:39:16 +0300

    validation

--- project/DTOs/Requests/CategoryRequest.cs
+++ project/DTOs/Requests/CategoryRequest.cs
@@ -1,7 +1,11 @@
-﻿namespace project.DTOs.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project.DTOs.Requests
 {
     public class CategoryRequest
     {
+        [Required(ErrorMessage ="Name is required")]
+        [MinLength(2)]
         public string Name { get; set; }
         public string Description { get; set; }
         public bool status { get; set; }
